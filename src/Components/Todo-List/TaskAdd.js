6b46456a7b724590bb047219f9d6d324d8aa8b03
Paras Bankoti <bankotibankoti@gmail.com>
2023-12-31 16:10:22 +0530

    delete button created for todo item

--- src/Components/Todo-List/TaskAdd.js
+++ src/Components/Todo-List/TaskAdd.js
@@ -1,12 +1,28 @@
 import React, { useState } from "react";
 import "./TaskAdd.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCheck, faTimes } from "@fortawesome/free-solid-svg-icons";
+import { v4 as uuidv4 } from "uuid";
 
 export const TaskAdd = () => {
   const [input, setInput] = useState("");
   const [todos, setTodos] = useState([]);
 
   const addTask = () => {
-    setTodos([...todos, input]);
+    const newId = uuidv4(); // Generate a unique id using uuid
+    setTodos([...todos, { id: newId, input }]);
+    setInput("");
+  };
+  console.log(todos);
+  const deleteTodo = (id) => {
+    const newTodoList = todos.filter((item) => item.id !== id);
+    console.log(id);
+    setTodos(newTodoList);
+  };
+
+  const doneTodo = (id) => {
+    // You can handle completed tasks here if needed
+    console.log("Task completed:", id);
   };
 
   return (
@@ -15,7 +31,7 @@ export const TaskAdd = () => {
         <input
           type="text"
           className="input-area"
-          //   value={input}
+          value={input}
           onChange={(e) => setInput(e.target.value)}
         />
         <button type="submit" className="btn-submit" onClick={addTask}>
@@ -24,13 +40,19 @@ export const TaskAdd = () => {
       </div>
       <div className="viewTodo">
         <ul>
-          {todos.map((val, id) => {
-            return (
-              <>
-                <div className="current-todo"></div>
-              </>
-            );
-          })}
+          {todos.map((val) => (
+            <div className="current-todo" key={val.id}>
+              <div className="eachTodo">{val.input}</div>
+              <FontAwesomeIcon
+                icon={faTimes}
+                onClick={() => deleteTodo(val.id)}
+              />
+              <FontAwesomeIcon
+                icon={faCheck}
+                onClick={() => doneTodo(val.id)}
+              />
+            </div>
+          ))}
         </ul>
       </div>
     </div>
